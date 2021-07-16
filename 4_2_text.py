b0c80a660e3674d0c9affd42e5ce0e2d75dd1167
egoing <egoing@gmail.com>
2021-07-16 16:47:48 +0900

    4.2. 문자열 데이터 타입

---
+++ 4_2_text.py
@@ -0,0 +1,11 @@
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+world
+''')
+
+print("'1'+'1'", '1'+'1')
+print('Hello world'*1000)
+print("len('Hello world'*1000)", len('Hello world'*1000))
+print("'Hello world'.replace('world', 'universe')", 'Hello world'.replace('world', 'universe'))
