1d7fd2097c8ca89846e537b3d34e0e55be9eec39
egoing <egoing@gmail.com>
2021-07-16 15:46:49 +0900

    4.1. 숫자형 데이터 타입

---
+++ 4_1_number.py
@@ -0,0 +1,16 @@
+print(-1)
+print(0)
+print(1) # int
+print(1.1) # float
+
+print('1+1', 1+1)
+print('2-1', 2-1)
+print('2*2', 2*2)
+print('4/2', 4/2)
+
+import math
+print('math.sqrt(4)', math.sqrt(4))
+print('math.pow(4,2)', math.pow(4,2))
+
+import random
+print('random.random()',random.random())
