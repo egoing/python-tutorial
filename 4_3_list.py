9401538087d0be29636285c7eb2a082f860495ef
egoing <egoing@gmail.com>
2021-07-17 12:09:21 +0900

    4.3. list data type

---
+++ 4_3_list.py
@@ -0,0 +1,13 @@
+students = ["egoing", "sori", "maru"]
+grades = [2,1,4]
+print("students[1]", students[1])
+print("len(students)", len(students))
+print("min(grades)", min(grades))
+print("min(grades)", max(grades))
+print("sum(grades)", sum(grades))
+
+import statistics
+print("statistics.mean(grades)", statistics.mean(grades))
+
+import random
+print("random.choice(students)", random.choice(students))
